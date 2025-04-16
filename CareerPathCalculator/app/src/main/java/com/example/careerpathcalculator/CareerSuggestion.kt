df741c4e57cde58abce5c89a883ded2fc1ad15ae
mohid786w <mohid.waseem@outlook.com>
2025-04-16 13:15:37 +0400

    Delete CareerPathCalculator directory

--- CareerPathCalculator/app/src/main/java/com/example/careerpathcalculator/CareerSuggestion.kt
+++
@@ -1,9 +0,0 @@
-package com.example.careerpathcalculator
-
-data class CareerSuggestion(
-    val title: String,
-    val description: String,
-    val quote: String,
-    val iconResourceId: Int,
-    val category: String
-)
